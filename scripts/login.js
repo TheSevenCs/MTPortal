6aa143b36e285cff5d529cef62f5b3f2cdcc4c5d
Andrew Lee <leeandrew135@gmail.com>
2024-05-14 20:04:30 -0400

    updated login page for http request

---
+++ scripts/login.js
@@ -0,0 +1,13 @@
+const loginProcess = Vue.createApp({
+  data() {
+    return {
+      usernameInput: "",
+      passwordInput: "",
+    };
+  },
+  methods: {
+    checkLogin() {
+      // COPY PASTE LOGIC HERE WITH ABOVE PARAMETERS
+    },
+  },
+});
