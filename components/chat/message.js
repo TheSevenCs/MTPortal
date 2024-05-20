5a59d7d4be93dac6815d4183b6f83d99e3922652
Yuval <117393676+TheSevenCs@users.noreply.github.com>
2024-05-19 22:04:32 -0400

    Almost done finishing the message component

--- components/chat/message.js
+++ components/chat/message.js
@@ -1,6 +1,21 @@
-application.component('message', {
-    template:
+application.component("message", {
+  props: {
+    message: {
+      required: true,
+    },
+  },
+  template:
     /*html*/
     `
-    `
-})
+    <div class="message">
+          <div class="avatar">
+            <img :src="message.avatarPath" alt="profile picture" />
+          </div>
+          <div class="message-content">
+            <span class="username">{{message.username}}</span>
+            <p class="">{{message.messageContent}}</p>
+          </div>
+        </div>
+    `,
+});
+app.mount("#client");
---
+++ scripts/chat.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      message: [
+        {
+          avatarPath: "./images/Sample_PFP.png",
+          messageContent: "I AM A FAT NIGGER",
+          username: "7C's",
+        },
+      ],
+    };
+  },
+});
