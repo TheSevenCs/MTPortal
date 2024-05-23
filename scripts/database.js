ab2232e0e9abe84713f139c7c7c4cc56fe41d03c
Yuval <117393676+TheSevenCs@users.noreply.github.com>
2024-05-23 19:42:50 -0400

    Made general functions

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+const { MongoClient } = require("mongodb");
 
 // MongoDB Connection URI
 const uri = "mongodb://ec2-3-144-255-131.us-east-2.compute.amazonaws.com:27017";
@@ -9,7 +9,17 @@ const dbName = "test";
 // Create a new MongoClient
 const client = new MongoClient(uri);
 
-async function run() {
+async function login(username, password) {
+  const accounts = getFromDatabase("accounts");
+  accounts;
+}
+
+async function createAccount(user, pass) {
+  const account = { username: user, password: pass };
+  await addToDatabase("accounts", account);
+}
+
+async function addToDatabase(collectionName, data) {
   try {
     // Connect to the MongoDB server
     await client.connect();
@@ -18,44 +28,39 @@ async function run() {
 
     const database = client.db(dbName);
 
-    // Perform operations with the database
-    // Example: Insert a document into a collection
-    const clients = database.collection("clients");
-    const users = database.collection("users");
+    // Get the specified collection
+    const collection = database.collection(collectionName);
 
-    console.log("Document inserted successfully");
+    // Insert the array of data into the collection
+    await collection.insertOne(data);
 
-    // Find all documents in the collection
-    const documents = await clients.find({}).toArray();
-    console.log("Documents in the collection:", documents);
+    console.log("Data inserted successfully into collection:", collectionName);
   } finally {
     // Close the client connection
     await client.close();
     console.log("Connection to MongoDB server closed");
   }
 }
+async function getFromDatabase(collectionName, filter = "") {
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
 
-async function login(username, password) {
-  await client.connect();
-  const database = client.db(dbName);
-  // Assuming `users` is the MongoDB collection object
-  const users = database.collection("users");
-  const account = await users.findOne({ username, password });
-  await client.close();
-  if (account != null) {
-    // Authentication successful
-    console.log("Documents in the collection:", account);
-    return account;
-  } else {
-    // Authentication failed
-    console.log("no users");
-    return null;
+    const database = client.db(dbName);
+
+    // Get the specified collection
+    const collection = database.collection(collectionName);
+
+    // Grab all data from the collection
+    const data = await collection.find(filter).toAqrray();
+    console.log(data);
+    // Return the array of data
+    return data;
+  } finally {
+    // Close the client connection
+    await client.close();
+    console.log("Connection to MongoDB server closed");
   }
 }
-
-async function createAccount(username, password) {
-  
-}
-
-run().catch(console.dir);
-login("user", "pass").catch(console.dir);
+// createAccount("user", "pass").catch(console.dir);
+getFromDatabase("accounts").catch(console.dir);
