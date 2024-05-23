e9fcbce86de5bd5c257a3eda722b49361a55dc79
Andrew Lee <leeandrew135@gmail.com>
2024-05-23 17:43:48 -0400

    updated events for add display

--- components/events/eventEntry.js
+++ components/events/eventEntry.js
@@ -3,9 +3,9 @@ eventsApp.component("component-event", {
   template:
     /*html*/
     `
-    <div class="event" :style="{ border: '2px solid ' + borderColor }" >
+    <div class="event" style="visibility: {{ displayStatus }}">
         <p class="text-event-title">{{ event.eventName }}</p>
-        <div class="text-event-date">Date: {{ event.eventDate }}</div>
+        <div class="text-event-date">{{ event.eventType }} Date: {{ event.eventDate }}</div>
         <p class="text-event-desc">Desc.: {{ event.eventDesc }}</p> 
         <button class="button-expand text-button-filter button-wrapper" @click="toggleModal">...</button>
     </div>
@@ -14,12 +14,12 @@ eventsApp.component("component-event", {
         <div class="modal-event-container">
             <!-- DISPLAY INFO HERE -->
             <p class="text-event-title" style="max-width: 88.5%;max-height: 15%; margin-top: 1vh">{{ event.eventName }}</p>
-            <div class="text-event-date" style="display: block; text-align:left; left: 25vw; margin-left: 1.5vh; margin-top: 0; max-width:45vw">Date: {{ event.eventDate }}</div>
+            <div class="text-event-date" style="display: block; text-align:left; left: 25vw; margin-left: 1.5vh; margin-top: 0; max-width:45vw">{{ event.eventType }} Date: {{ event.eventDate }}</div>
             <p class="text-event-desc" style="margin-top:5vh; margin-left:1.5vh; max-width:50vw; max-height: 39vh">Desc.: {{ event.eventDesc }}</p>
 
             <!-- MODAL MANAGE/DELETE EVENT -->
-            <button class="button-filter text-button-filter button-wrapper" style="right: 54.17vw;bottom:17vh;margin-top:2vh">Edit Event</button>
-            <button class="button-filter text-button-filter button-wrapper" style="right: 33.3vw;bottom:17vh;margin-top:2vh">Delete Event</button>
+            <button @click="" class="button-filter text-button-filter button-wrapper" style="right: 54.17vw;bottom:17vh;margin-top:2vh">Edit Event</button>
+            <button @click="deleteEvent" class="button-filter text-button-filter button-wrapper" style="right: 33.3vw;bottom:17vh;margin-top:2vh">Delete Event</button>
 
             <!-- MODAL EXIT -->
             <button class="button-expand text-button-filter button-wrapper" @click="toggleModal" style="padding-bottom: 0; top: 17vh; right: 25.5vw;">X</button>
@@ -29,7 +29,9 @@ eventsApp.component("component-event", {
     `,
   data() {
     return {
+      displayEvent: true,
       displayModal: false,
+      displayStatus: "visible",
     };
   },
   methods: {
@@ -40,6 +42,10 @@ eventsApp.component("component-event", {
     selectEvent() {
       this.eventSelected = !this.eventSelected;
       this.borderColor = this.eventSelected ? "#FF0000" : "white";
+    },
+    deleteEvent() {
+      this.displayEvent = !this.displayEvent;
+      this.displayEvent = this.displayEvent ? "visible" : "hidden";
     },
   },
 });
--- scripts/events.js
+++ scripts/events.js
@@ -5,55 +5,67 @@ const eventsApp = Vue.createApp({
         {
           eventName: "long description tester inside the modal",
           eventDate: "Today",
+          eventType: "Meeting",
           eventDesc:
             "Description for Event1. Random text for testing how long the text box should be and how im going to wrap the text around this is so fun ha ha ha im really mentally stable guys i promise but if the text keeps on going then we are going ot have a bit of a problem on our hands am i right guys its almost like those 56%ers if you catch my drift he he he so now you can see that it goes up until it reaches the next event component Description for Event1. Random text for testing how long the text box should be and how im going to wrap the text around this is so fun ha ha ha im really mentally stable guys i promise but if the text keeps on going then we are going ot have a bit of a problem on our hands am i right guys its almost like those 56%ers if you catch my drift he he he so now you can see that it goes up until it reaches the next event component Description for Event1. Random text for testing how long the text box should be and how im going to wrap the text around this is so fun ha ha ha im really mentally stable guys i promise but if the text keeps on going then we are going ot have a bit of a problem on our hands am i right guys its almost like those 56%ers if you catch my drift he he he so now you can see that it goes up until it reaches the next event component Description for Event1. Random text for testing how long the text box should be and how im going to wrap the text around this is so fun ha ha ha im really mentally stable guys i promise but if the text keeps on going then we are going ot have a bit of a problem on our hands am i right guys its almost like those 56%ers if you catch my drift he he he so now you can see that it goes up until it reaches the next event component",
         },
         {
           eventName: "eventName2",
           eventDate: "Tomorrow",
+          eventType: "Meeting",
           eventDesc: "Description for Event2.",
         },
         {
           eventName: "eventName3",
           eventDate: "The day after",
+          eventType: "Meeting",
           eventDesc: "Description for Event3.",
         },
         {
           eventName: "eventName4",
           eventDate: "1/4/2024",
+          eventType: "Meeting",
           eventDesc: "Description for Event4.",
         },
         {
           eventName: "eventName5",
           eventDate: "1/5/2024",
+          eventType: "Meeting",
           eventDesc: "Description for Event5.",
         },
         {
           eventName: "eventName6",
           eventDate: "1/6/2024",
+          eventType: "Meeting",
           eventDesc: "Description for Event6.",
         },
       ],
       newEventName: "",
       newEventDate: "",
+      newEventType: "",
       newEventDesc: "",
       displayAddModal: false,
     };
   },
 
   methods: {
-    addNewEvent(newName, newDate, newDesc) {
+    addNewEvent(newName, newDate, newType, newDesc) {
       const newEvent = {
         eventName: newName,
         eventDate: newDate,
+        eventType: newType,
         eventDesc: newDesc,
       };
 
       // ADD TO ARRAY
       this.events.push(newEvent);
+
+      // RESET VARIABLES
       this.newEventName = "";
       this.newEventDate = "";
+      this.newEventType = "";
       this.newEventDesc = "";
+      this.displayAddModal = false;
     },
     toggleAddModal() {
       this.displayAddModal = !this.displayAddModal;
