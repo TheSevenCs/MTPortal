76e4ff1fc3fbdc637b1377ddd18796a57ae4416a
Andrew Lee <leeandrew135@gmail.com>
2024-05-24 21:48:36 -0400

    shat.js update

--- scripts/chat.js
+++ scripts/chat.js
@@ -9,6 +9,40 @@ const app = Vue.createApp({
           username: "7C's",
         },
       ],
+      newAvaterPath: "",
+      newMessageContent: "",
+      newUsername: "",
     };
+  },
+  methods: {
+    addNewMessage() {
+      axios
+        .post(this.addMessage)
+        .then((response) => {
+          console.log("Message added successfully: ", response.data);
+        })
+        .catch((error) => {
+          console.error("Error adding message: ", error);
+        });
+
+      // RESET VARIABLES
+      this.newAvaterPath = "";
+      this.newMessageContent = "";
+      this.newUsername = "";
+
+      // ADD DELAY IF NEEDED
+    },
+  },
+  computed: {
+    addMessage() {
+      return (
+        "/addMessage?newAvatarPath=" +
+        this.newAvaterPath +
+        "&newMessageContent=" +
+        this.newMessageContent +
+        "&newUsername=" +
+        this.newUsername
+      );
+    },
   },
 });
