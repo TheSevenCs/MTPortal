6ae7526e03685213c52f3fbef9b98bc0ff578583
Yuval <117393676+TheSevenCs@users.noreply.github.com>
2024-06-10 20:35:42 -0400

    Added a 6 day expire time

--- DataAccess/localDA.js
+++ DataAccess/localDA.js
@@ -3,9 +3,15 @@ const fs = require("fs");
 const accountCachePath = "./cache/cache.json";
 
 function saveAccountKey(accountKey) {
-  // Create an object to store the account key
+  // Calculate the expiration timestamp
+  const expirationTimeInDays = 6;
+  const expirationTimeInSeconds = expirationTimeInDays * 60 * 60 * 24;
+  const expirationTimestamp = Date.now() + expirationTimeInSeconds;
+
+  // Create an object to store the account key and expiration timestamp
   const data = {
     accountKey: accountKey,
+    expirationTimestamp: expirationTimestamp,
   };
 
   // Convert the object to a JSON string
@@ -21,13 +27,31 @@ function saveAccountKey(accountKey) {
   });
 }
 
+// Function to retrieve the account key from the JSON file and check expiration
 function getAccountKey() {
   try {
+    // Check if the file exists
+    if (!fs.existsSync(accountCachePath)) {
+      return "no-file";
+    }
+
     // Read the contents of the JSON file
     const jsonData = fs.readFileSync(accountCachePath, "utf8");
 
+    // Check if the file is empty or doesn't contain valid JSON data
+    if (!jsonData.trim()) {
+      return null;
+    }
+
     // Parse the JSON data
     const data = JSON.parse(jsonData);
+    // Check if the key has expired
+    const expirationTimestamp = data.expirationTimestamp;
+    if (expirationTimestamp && Date.now() > expirationTimestamp) {
+      // Invalidate the key by removing it from the file
+      fs.unlinkSync(accountCachePath);
+      return "expired";
+    }
 
     // Access the key from the JSON data
     const accountKey = data.accountKey;
@@ -38,7 +62,6 @@ function getAccountKey() {
     return null;
   }
 }
-
 module.exports = {
   saveAccountKey: saveAccountKey,
   getAccountKey: getAccountKey,
